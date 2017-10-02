639233c5a47074d5d65ac09be406e7ec2a1cf410
JimmyLee05 <NanJun05@163.com>
2017-10-02 20:05:25 +0800

    add new files

--- MLDemo/ViewController.swift
+++ MLDemo/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    let model = Inceptionv3()
+    
+    @IBAction func takePhotoButtonTapped() {
+        let picker              = UIImagePickerController()
+        picker.delegate         = self
+        picker.allowsEditing    = true
+        picker.sourceType       = .photoLibrary
+        present(picker, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func analyzeImage(_ image: CVPixelBuffer) -> String {
+        do {
+            let prediction = try model.prediction(image: image).classLabel
+            return prediction
+        } catch {
+            return "something wrong..."
+        }
     }
-
-
+    
+    func resizeImage(_ input: UIImage) -> UIImage {
+        UIGraphicsBeginImageContext(CGSize(width: 299, height: 299))
+        input.draw(in:CGRect(x: 0, y: 0, width: 299, height: 299))
+        let output  = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return output!
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        guard let image         = info["UIImagePickerControllerEditedImage"] as? UIImage else { return }
+        let resizedImage        = resizeImage(image)
+        guard let pixelBuffer   = resizedImage.toPixelBuffer() else { return }
+        let prediction          = analyzeImage(pixelBuffer)
+        
+        imageView.image         = image
+        label.text              = prediction
+    }
 }
 
